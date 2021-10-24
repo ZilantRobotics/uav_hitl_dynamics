97a9ccebab416a245f44814ea946f11822d73b32
Roman Fedorenko <frontwise@gmail.com>
2021-10-24 14:52:54 +0300

    Copy inno_vtol_dynamics from innopolis_vtol_dynamics (making submodule)

---
+++ src/dynamics/uavDynamicsSimBase.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file uavDynamicsSimBase.cpp
+ * @author ponomarevda96@gmail.com
+ */
+
+#include "uavDynamicsSimBase.hpp"
+
+bool UavDynamicsSimBase::getMotorsRpm(std::vector<double>& motorsRpm) {
+    return false;
+}
