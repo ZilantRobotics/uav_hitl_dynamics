1cb691c1dcf9f40c12214d4b1931ee26c7c2f83a
ponomarevda <ponomarevda96@gmail.com>
2022-04-29 16:34:08 +0300

    move sensors implementation from node into sensors file, fix weird implementation of cs_convertion

--- include/sensors.hpp
+++ include/sensors.hpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <ros/time.h>
+#include <Eigen/Geometry>
+#include <random>
+#include <geographiclib_conversions/geodetic_conv.hpp>
 
 class BaseSensor{
     public:
@@ -22,6 +25,21 @@ class BaseSensor{
         const double PERIOD;
         ros::Publisher publisher_;
         double nextPubTimeSec_ = 0;
+
+        std::default_random_engine randomGenerator_;
+        std::normal_distribution<double> normalDistribution_{std::normal_distribution<double>(0.0, 1.0)};
+};
+
+class AttitudeSensor : public BaseSensor{
+    public:
+        AttitudeSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(const Eigen::Quaterniond& attitudeFrdToNed);
+};
+
+class BatteryInfoStatusSensor : public BaseSensor{
+    public:
+        BatteryInfoStatusSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(double rpm);
 };
 
 class EscStatusSensor : public BaseSensor{
@@ -32,22 +50,59 @@ class EscStatusSensor : public BaseSensor{
         uint8_t nextEscIdx_ = 0;
 };
 
-class IceStatusSensor : public BaseSensor{
-    public:
-        IceStatusSensor(ros::NodeHandle* nh, const char* topic, double period);
-        bool publish(double rpm);
-};
-
 class FuelTankStatusSensor : public BaseSensor{
     public:
         FuelTankStatusSensor(ros::NodeHandle* nh, const char* topic, double period);
         bool publish(double rpm);
 };
 
-class BatteryInfoStatusSensor : public BaseSensor{
+class GpsSensor : public BaseSensor{
     public:
-        BatteryInfoStatusSensor(ros::NodeHandle* nh, const char* topic, double period);
+        GpsSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(const Eigen::Vector3d& gpsPosition, const Eigen::Vector3d& nedVelocity);
+};
+
+class IceStatusSensor : public BaseSensor{
+    public:
+        IceStatusSensor(ros::NodeHandle* nh, const char* topic, double period);
         bool publish(double rpm);
 };
+
+class ImuSensor : public BaseSensor{
+    public:
+        ImuSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(const Eigen::Vector3d& accFrd, const Eigen::Vector3d& gyroFrd);
+};
+
+class MagSensor : public BaseSensor{
+    public:
+        MagSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(const Eigen::Vector3d& geoPosition, const Eigen::Quaterniond& attitudeFrdToNed);
+};
+
+class RawAirDataSensor : public BaseSensor{
+    public:
+        RawAirDataSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(float absPressureHpa, float diffPressure, float staticTemperature);
+};
+
+class StaticPressureSensor : public BaseSensor{
+    public:
+        StaticPressureSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(float staticPressureHpa);
+};
+
+class StaticTemperatureSensor : public BaseSensor{
+    public:
+        StaticTemperatureSensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(float staticTemperature);
+};
+
+class VelocitySensor : public BaseSensor{
+    public:
+        VelocitySensor(ros::NodeHandle* nh, const char* topic, double period);
+        bool publish(const Eigen::Vector3d& linVelNed, const Eigen::Vector3d& angVelFrd);
+};
+
 
 #endif  // INNO_VTOL_DYNAMICS_SENSORS_HPP
